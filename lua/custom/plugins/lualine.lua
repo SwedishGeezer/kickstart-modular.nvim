59fc09d75cf4c3ca3e4ac03dd14445c2009ff7f9
Swedish Geezer <bondlurken97@gmail.com>
2024-04-02 13:09:45 +0200

    Lualine config & devicons added

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,11 +1,30 @@
 return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
       options = {
         icons_enabled = true,
         theme = 'solarized-osaka',
+        component_separators = '',
+        sections_separators = '',
+        always_divide_middle = true,
+        globalstatus = true,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        },
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { 'filename', 'diagnostics' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      extensions = { 'neo-tree' },
     }
   end,
 }
