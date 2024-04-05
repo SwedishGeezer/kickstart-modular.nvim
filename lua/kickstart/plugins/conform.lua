d371bd5c15557c9dabcef639608120ca1e44f1d2
Swedish Geezer <bondlurken97@gmail.com>
2024-04-06 00:12:13 +0200

    Added prettier to some langs

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -20,7 +20,11 @@ return {
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        css = { { 'prettierd', 'prettier' } },
+        html = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        htmx = { { 'prettierd', 'prettier' } },
       },
     },
   },
